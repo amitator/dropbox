5d741c24aa6512f35e435815ff4c73e22fdfc9c8
Igor Prus <prus.igor@gmail.com>
2018-05-26 20:44:08 +0300

    moved files to the packet

---
+++ server/src/main/java/ru/geekbrains/ClientHandler.java
@@ -0,0 +1,103 @@
+package ru.geekbrains;
+
+
+import main.java.ru.geekbrains.*;
+
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientHandler {
+    private Server server;
+    private Socket socket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private String login;
+    private boolean isAuthorized;
+
+    public ClientHandler (final Server server, final Socket socket){
+        try {
+            this.server = server;
+            this.socket = socket;
+            this.in = new ObjectInputStream(socket.getInputStream());
+            this.out = new ObjectOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                isAuthorized = false;
+                try {
+                    while (true){
+                        AbstractMessage abstractMessage = (AbstractMessage) in.readObject();
+                        if (abstractMessage instanceof AuthMessage){
+                            AuthMessage am = (AuthMessage) abstractMessage;
+                            isAuthorized = SqlHandler.isAuthorized(am.getLogin(), am.getPassword());
+                            if (isAuthorized){
+                                this.login = am.getLogin();
+                                sendMessage(new CommandMessage(CommandMessage.AUTH_OK, login));
+                                sendMessage(getFileStructureMessage());
+                                break;
+                            }
+                        }
+                    }
+                    while (true){
+                        AbstractMessage abstractMessage = (AbstractMessage) in.readObject();
+                        if (abstractMessage instanceof FileDataMessage) {
+                            FileDataMessage fdm = (FileDataMessage) abstractMessage;
+                            Files.write(Paths.get(login + "/" + fdm.getFileName()), fdm.getData(), StandardOpenOption.CREATE_NEW);
+                        }
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    try {
+                        in.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    try {
+                        out.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    System.out.println("Client disconnected.");
+                }
+            }).start();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public FileListMessage getFileStructureMessage(){
+        return new FileListMessage(getFilesList());
+    }
+
+    public List<String> getFilesList(){
+        List<String> files = new ArrayList<>();
+        try {
+            Files.newDirectoryStream(Paths.get(login)).forEach(path -> files.add(path.getFileName().toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return files;
+    }
+
+    public void sendMessage(AbstractMessage abstractMessage){
+        try {
+            out.writeObject(abstractMessage);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ server/src/main/java/ru/geekbrains/SqlHandler.java
@@ -0,0 +1,50 @@
+package ru.geekbrains;
+
+import java.sql.*;
+
+public class SqlHandler {
+    private static Connection connection;   //we need only 1 connection so can make it static
+    private static Statement statement;
+
+    public static void connect() throws ClassNotFoundException, SQLException { //throw them out otherwise noone would know if something goes wrong
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:server/data.db");
+        statement = connection.createStatement();
+//        prepare();
+        System.out.println("DB connected!");
+    }
+
+    private static void prepare() {
+        try {
+            statement.executeUpdate("CREATE TABLE IF NOT EXIST users (id       INTEGER PRIMARY KEY AUTOINCREMENT, login    TEXT    UNIQUE, password TEXT)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void disconnect(){
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        try {
+            statement.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static boolean isAuthorized(String login, String pass){
+        try {
+            ResultSet rs = statement.executeQuery(String.format(
+                    "SELECT login FROM users WHERE login = '%s' AND password = '%s';", login, pass));
+            if (rs.next()) {
+                return true;
+            }
+        }catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+}
